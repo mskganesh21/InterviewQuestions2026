4d0c1febf6b8a07578690da58acdb71aecf07094
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2026-03-16 12:28:02 +0530

    Document immutability in JavaScript with examples
    Added comments explaining immutability in JavaScript and provided examples.

--- javascriptSpecialInterviewQuestions.js
+++ javascriptSpecialInterviewQuestions.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+/*
+IMMUTABILITY
+immutability means that the data or object cannot be altered after creation.any change creates a new one instead.
+
+eg: primitive data types (string,numbers,boolean) are immutable in javascript 
+*/
+
+let str = "hello";
+str[0] = "H";
+
+console.log(str);
+/*even if we change the value to uppercase it didn't change this is immutability*/
+
+/*
+strings are immutable for thread safety, performance optimization and safety 
+*/
