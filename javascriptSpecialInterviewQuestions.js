a862faf1089e3ccba9d31611694eaf8c36a1c42d
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2026-03-17 15:17:58 +0530

    Document prototypal inheritance in JavaScript
    Added explanation of prototypal inheritance in JavaScript, including examples of how objects inherit properties from their prototypes.

--- javascriptSpecialInterviewQuestions.js
+++ javascriptSpecialInterviewQuestions.js
@@ -487,3 +487,23 @@ var initialiseClosure = randomFunc(); // Returns a function
 
 initialiseClosure();  //when this function runs we'll still get the output as vivian is awesome 
 //this ability is called closure 
+
+
+/*PROTOTYPAL INHERITANCE IN JAVASCRIPT*/
+All javascript objects inherit properties from a prototype. For example,
+Date objects inherit properties from the Date prototype
+Math objects inherit properties from the Math prototype
+Array objects inherit properties from the Array prototype.
+On top of the chain is Object.prototype. Every prototype inherits properties and methods from the Object.prototype.
+A prototype is a blueprint of an object. The prototype allows us to use properties and methods on an object even if the properties and methods do not exist on the current object.
+
+Consider an array 
+let arr = [];
+arr.push(2);
+console.log(arr);
+
+In the code above, as one can see, we have not defined any property or method called push on the array “arr” but the javascript engine does not throw an error.
+The reason is the use of prototypes. As we discussed before, Array objects inherit properties from the Array prototype.
+The javascript engine sees that the method push does not exist on the current array object and therefore, looks for the method push inside the Array prototype and it finds the method.
+
+this is prototypal inheritance in javascript
